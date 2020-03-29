ebe6c09a7bbe65e4abd3c2cb73091441e3253936
rockethyperlocal <mapmydestination@gmail.com>
2020-03-29 16:10:09 +0530

    update

--- index.php
+++ index.php
@@ -5,13 +5,6 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
 	</head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
-	<div class="col-md-3"></div>
-	<div class="col-md-6 well">
 		<h3 class="text-primary">PHP - Convert HTML To PDF</h3>
 		<hr style="border-top:1px dotted #ccc;"/>
 		<button type="button" class="btn btn-success" data-toggle="modal" data-target="#form_modal"><span class="glyphicon glyphicon-plus"></span> Add member</button>
@@ -83,7 +76,6 @@
 				</div>
 			</form>
 		</div>
-	</div>
 </body>
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
